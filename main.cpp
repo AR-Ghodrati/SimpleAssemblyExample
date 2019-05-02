709ad0d10a311396372616ff6920846df875aadc
alirezaghodrati <apps.ghodrati1377@gmail.com>
2019-05-02 12:06:48 +0430

    Update C++ Tester

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ extern "C" {
     int MUL7(int arg0);
     int FIND_PATTERN_1011(int arg0);
     void REVERSE(int* arg0 , int arg1);
+    
+    int Counter1bit(int Number , int RegSize);
+    int DivisibilityCounter(int* Array, int ArraySize, int Value);
+    int LinearSearch(int* Array, int ArraySize, int Value);
+    
 
 }
 
